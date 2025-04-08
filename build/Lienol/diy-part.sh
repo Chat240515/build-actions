4aefba59df46312114ebff0b59fbe7074df372f0
bilibili20001 <70944645+bilibili20001@users.noreply.github.com>
2025-04-08 11:48:21 +0800

    Update 24.10

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -68,10 +68,10 @@ export kernel_usage="stable"
 
 
 # 添加软件包
-sed -i 'li src-git NueXini https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '1i src-git luciapp https://github.com/Chat240515/luciapp' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages;main' >>feeds.conf.default
 
 
