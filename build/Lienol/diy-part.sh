800b41116e69034c19a7caf4d7748d8fa9354ab9
bilibili20001 <70944645+bilibili20001@users.noreply.github.com>
2025-04-08 10:06:56 +0800

    Update 24.10

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -70,7 +70,7 @@ export kernel_usage="stable"
 # 添加软件包
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone https://github.com/openwrt-develop/luci-app-vlmcsd package/luci-app-vlmcsd
+git clone https://github.com/siwind/luci-app-vlmcsd package/luci-app-vlmcsd
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages;main' >>feeds.conf.default
 
