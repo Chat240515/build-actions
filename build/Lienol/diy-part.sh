07e9bba3a1cc738698483445e591be8b808f9029
bilibili20001 <70944645+bilibili20001@users.noreply.github.com>
2025-04-09 09:03:55 +0800

    Update 24.10

--- build/Lienol/diy-part.sh
+++ build/Lienol/diy-part.sh
@@ -70,8 +70,8 @@ export kernel_usage="stable"
 # 添加软件包
 # sed -i '1i src-git immortalwrt https://github.com/immortalwrt/luci' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-git clone https://github.com/Chat240515/luciapp package/luci-app-vlmcsd
 git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+echo 'src-git vlmcsd https://github.com/Chat240515/luciapp' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages;main' >>feeds.conf.default
 
